e7e846258c72c796f82d74909252b621664440a5
Kevin Hwang <k3vinhwang@gmail.com>
2014-08-26 00:03:17 -0700

    Get EJS

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ var express = require("express");
 
 var app = express();
 
+// Configuration
+app.use("/static", express.static(__dirname + "/public"));
+
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Hello world!");
+  res.render("index");
 });
 
-app.listen(3000);
+var server = app.listen(3000, function () {
+  console.log("Listening on port %d", server.address().port);
+});
